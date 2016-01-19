615d5fcfc16f540983ad6bfe9b22a639dc56eb8b
lionlone <botxabong009@gmail.com>
2016-01-19 12:02:11 +0700

    Update upload.php

--- controller/upload.php
+++ controller/upload.php
@@ -1,8 +1,9 @@
 <?php
-        $file_path = $_FILES['fileUpload']['tmp_name'];
-        $file_name = $_FILES['fileUpload']['name'];
-        $to = "../upload/img/" . $file_name;
-        move_uploaded_file($file_path, $to);
-    echo $file_name;
-    include"../views/upload.php";
+if(isset($_FILES["fileUpload"])){
+    $file_path = $_FILES['fileUpload']['tmp_name'];
+    $file_name = $_FILES['fileUpload']['name'];
+    $to = "../upload/img/" . $file_name;
+    move_uploaded_file($file_path, $to);
+}
+include"../views/upload.php";
 ?>
