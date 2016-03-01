3fe52e8980d5b08a275d0d76c13385a88f8299cd
lionlone <botxabong009@gmail.com>
2016-03-01 11:48:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,9 @@
     include"database.php";
     include"controller/controller.php";
 ?>
+http://www.techrepublic.com/article/determine-class-type-with-these-10-php-variable-testing-functions/
+check type of variable php
+https://github.com/Nimrod007/PHP_image_resize/blob/master/smart_resize_image.function.php
+http://www.nimrodstech.com/php-image-resize/
+resize image php code
+http://code.tutsplus.com/tutorials/image-resizing-made-easy-with-php--net-10362
